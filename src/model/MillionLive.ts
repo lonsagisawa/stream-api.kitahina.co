0966c7d5b8462c76467dc8e5d7e38db5e7fe52e5
Lon Sagisawa <lon@sagisawa.me>
2023-01-12 00:54:17 +0900

    MillionLive: THE@TER WAVE

--- src/model/MillionLive.ts
+++ src/model/MillionLive.ts
@@ -1,10 +1,12 @@
 import { Album } from "../types/Album.ts";
 import MillionRadio from "./millionlive/MillionRadio.ts";
+import MillionTheaterWave from "./millionlive/MillionTheaterWave.ts";
 
 const Albums: Array<Album> = [];
 
 const MillionLive: Array<Album> = Albums.concat(
   MillionRadio,
+  MillionTheaterWave,
 );
 
 export default MillionLive;
