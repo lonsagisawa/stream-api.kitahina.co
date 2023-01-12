9f98f81394850afcc255fc16972913b0a94c09d5
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-01-13 00:53:06 +0900

    add live performance album 01-03

---
+++ src/model/millionlive/MillionTheaterPerformance.ts
@@ -0,0 +1,31 @@
+import { Album } from "../../types/Album";
+
+const MillionTheaterPerformance: Array<Album> = [
+  {
+    id: "lacm-14080",
+    name: "THE IDOLM@STER LIVE THE@TER PERFORMANCE 01 Thank You!",
+    series: "millionlive",
+    cover: "https://www.lantis.jp/imas/img/release_imgs__0011s_0012_LTP01_H1.png",
+    spotify: "1u4OlClvfSusqtWsi5mxlJ",
+    applemusic: "1658188099",
+    amazon: "B0BP4XVG26",
+  },
+  {
+    id: "laca-15300",
+    name: "THE IDOLM@STER LIVE THE@TER PERFORMANCE 02",
+    series: "millionlive",
+    cover: "https://www.lantis.jp/imas/img/release_imgs__0011s_0011_LTP02_H1.png",
+    spotify: "63Ic75x61p1Mtdcyt8roK5",
+    applemusic: "1658389193",
+    amazon: "B0BP8HJ5FQ",
+  },
+  {
+    id: "laca-15313",
+    name: "THE IDOLM@STER LIVE THE@TER PERFORMANCE 03",
+    series: "millionlive",
+    cover: "https://www.lantis.jp/imas/img/release_imgs__0011s_0010_LTP03_H1.png",
+    spotify: "4PRL12x0kNJeuWfzfBURU6",
+    applemusic: "1658183832",
+    amazon: "B0BP5SWJ8S",
+  }
+];
