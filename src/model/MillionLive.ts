9a8ff5dd6c88150882087932f02d8563aaf16d82
Lon Sagisawa <lon@sagisawa.me>
2023-01-13 19:54:23 +0900

    Million Live: M@ster Sparkle 2

--- src/model/MillionLive.ts
+++ src/model/MillionLive.ts
@@ -2,13 +2,15 @@ import { Album } from "../types/Album.ts";
 import MillionRadio from "./millionlive/MillionRadio.ts";
 import LiveTheaterPerformance from "./millionlive/LiveTheaterPerformance.ts";
 import MillionTheaterWave from "./millionlive/MillionTheaterWave.ts";
+import MasterSparkle2 from "./millionlive/MasterSparkle2.ts";
 
 const Albums: Array<Album> = [];
 
 const MillionLive: Array<Album> = Albums.concat(
+  LiveTheaterPerformance,
   MillionRadio,
   MillionTheaterWave,
-  LiveTheaterPerformance,
+  MasterSparkle2,
 );
 
 export default MillionLive;
