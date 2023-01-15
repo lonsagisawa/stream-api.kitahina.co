b4318144a5a478fed5493848790df9c3fab92088
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-01-15 12:25:16 +0900

    add export for brand model

--- src/model/MillionLive.ts
+++ src/model/MillionLive.ts
@@ -3,12 +3,14 @@ import MillionRadio from "./millionlive/MillionRadio.ts";
 import LiveTheaterPerformance from "./millionlive/LiveTheaterPerformance.ts";
 import MillionTheaterWave from "./millionlive/MillionTheaterWave.ts";
 import MasterSparkle2 from "./millionlive/MasterSparkle2.ts";
+import MillionTheaterGeneration from "./millionlive/MillionTheaterGeneration.ts";
 
 const Albums: Array<Album> = [];
 
 const MillionLive: Array<Album> = Albums.concat(
   LiveTheaterPerformance,
   MillionRadio,
+  MillionTheaterGeneration,
   MillionTheaterWave,
   MasterSparkle2,
 );
