a7162c364ebf176edf7d689356a0309bc3957cfc
Lon Sagisawa <lon@sagisawa.me>
2023-01-31 23:52:11 +0900

    Shiny Colors: Add 神様は死んだ、って

--- src/model/ShinyColors.ts
+++ src/model/ShinyColors.ts
@@ -2,6 +2,7 @@ import { Album } from "../types/Album.ts";
 import ColorfulFeathers from "./shinycolors/2021_ColorfulFeathers.ts";
 import SyntheSide from "./shinycolors/2021_SyntheSide.ts";
 import PanoramaWing from "./shinycolors/2022_PanoramaWing.ts";
+import Other from "./shinycolors/Other.ts";
 
 const Albums: Array<Album> = [];
 
@@ -9,6 +10,7 @@ const ShinyColors: Array<Album> = Albums.concat(
   ColorfulFeathers,
   SyntheSide,
   PanoramaWing,
+  Other,
 );
 
 export default ShinyColors;
---
+++ src/model/shinycolors/Other.ts
@@ -0,0 +1,17 @@
+import { Album } from "../../types/Album.ts";
+
+const Other: Array<Album> = [
+  /* P@NORAMA WING / 2022 */
+  {
+    id: "lzc-2059",
+    name: "神様は死んだ、って",
+    brand: "shinycolors",
+    cover:
+      "https://shinycolors.lantis.jp/X9pa5jNY/wp-content/uploads/2021/12/LACM-24251.jpeg",
+    spotify: "6YOFCgbDa6mUrXjiRufDXT",
+    applemusic: "1655632835",
+    amazon: "B0BN87VC1C",
+  },
+];
+
+export default Other;
