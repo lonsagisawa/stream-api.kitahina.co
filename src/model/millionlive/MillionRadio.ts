75d83a7f989f7d2584cd3b645b5debe850c642b3
Lon Sagisawa <lon@sagisawa.me>
2023-02-08 00:48:52 +0900

    ML: new data structure for million radio

--- src/model/millionlive/MillionRadio.ts
+++ src/model/millionlive/MillionRadio.ts
@@ -5,30 +5,39 @@ const MillionRadio: Array<Album> = [
     id: "lacm-14234",
     name: "U・N・M・E・I ライブ",
     brand: "millionlive",
+    series: "MILLION RADIO!",
     cover:
       "https://www.lantis.jp/imas/img/release_imgs__0002s_0000_miriradi_t_H1.png",
-    spotify: "5r4MPB6kVwgvZEXzPRGvLt",
-    applemusic: "1658393012",
-    amazon: "B0BP8L3H1Z",
+    platform: {
+      spotify: "5r4MPB6kVwgvZEXzPRGvLt",
+      applemusic: "1658393012",
+      amazon: "B0BP8L3H1Z",
+    },
   },
   {
     id: "lacm-14506",
     name: "ターンオンタイム！",
     brand: "millionlive",
+    series: "MILLION RADIO!",
     cover:
       "https://www.lantis.jp/imas/img/release_imgs__0001s_0000_million_radio_cd2_jk.png",
-    spotify: "5vMQLGCHrOk4L2B4RWMdkY",
-    applemusic: "1658286333",
-    amazon: "B0BP7SC3PR",
+    platform: {
+      spotify: "5vMQLGCHrOk4L2B4RWMdkY",
+      applemusic: "1658286333",
+      amazon: "B0BP7SC3PR",
+    },
   },
   {
     id: "lacm-14961",
     name: "ENDLESS TOUR",
     brand: "millionlive",
+    series: "MILLION RADIO!",
     cover: "https://www.lantis.jp/imas/img/LACM-14961.jpg",
-    spotify: "3JgVLrqgyLopz9fofSGVU7",
-    applemusic: "1658220574",
-    amazon: "B0BP6H76CJ",
+    platform: {
+      spotify: "3JgVLrqgyLopz9fofSGVU7",
+      applemusic: "1658220574",
+      amazon: "B0BP6H76CJ",
+    },
   },
 ];
 
