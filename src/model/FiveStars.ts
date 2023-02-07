db81e48e93d79a1bc89f2f9d348e8e9019a6612e
Lon Sagisawa <lon@sagisawa.me>
2023-02-08 00:20:15 +0900

    New data structure for FIVE STARS songs

--- src/model/FiveStars.ts
+++ src/model/FiveStars.ts
@@ -6,27 +6,33 @@ const FiveStars: Array<Album> = [
     name: "IDOL POWER RAINBOW",
     brand: "fivestars",
     cover: "https://www.lantis.jp/imas/img/TC01_H1.jpg",
-    spotify: "6SScLq8G2nr8wVd3tI8B0N",
-    applemusic: "1665511914",
-    amazon: "B0BS9MZPKJ",
+    platform: {
+      spotify: "6SScLq8G2nr8wVd3tI8B0N",
+      applemusic: "1665511914",
+      amazon: "B0BS9MZPKJ",
+    },
   },
   {
     id: "moiw2015",
     name: "アイ MUST GO!",
     brand: "fivestars",
     cover: "https://www.lantis.jp/imas/img/TC02_H1.jpg",
-    spotify: "4X3xDnRuKxje0wHFYkreCo",
-    applemusic: "1665511829",
-    amazon: "B0BS9P3WDP",
+    platform: {
+      spotify: "4X3xDnRuKxje0wHFYkreCo",
+      applemusic: "1665511829",
+      amazon: "B0BS9P3WDP",
+    },
   },
   {
     id: "moiw2023",
     name: "CRYST@LOUD",
     brand: "fivestars",
     cover: "https://www.lantis.jp/imas/img/TC03_H1.jpg",
-    spotify: "0RiOQYiFCzGqBwqPIC1SXa",
-    applemusic: "1664668294",
-    amazon: "B0BS9MF3WD",
+    platform: {
+      spotify: "0RiOQYiFCzGqBwqPIC1SXa",
+      applemusic: "1664668294",
+      amazon: "B0BS9MF3WD",
+    },
   },
 ];
 
