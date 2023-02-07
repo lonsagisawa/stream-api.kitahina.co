9726c40bb75e0ae8ce06d74cd884bf8ba39f2952
Lon Sagisawa <lon@sagisawa.me>
2023-02-08 00:30:57 +0900

    ML: new data structure for LTF

--- src/model/millionlive/LiveTheaterForward.ts
+++ src/model/millionlive/LiveTheaterForward.ts
@@ -5,71 +5,92 @@ const LiveTheaterForward: Array<Album> = [
     id: "laca-15611",
     name: "THE IDOLM@STER LIVE THE@TER FORWARD 01 Sunshine Rhythm",
     brand: "millionlive",
+    series: "LIVE THE@TER FORWARD",
     cover:
       "https://www.lantis.jp/imas/img/release_imgs__0007s_0002_LTF01_H1.png",
-    spotify: "5AI4ZKsvVtfMTj0TknJjBE",
-    applemusic: "1658287059",
-    amazon: "B0BP837KCY",
+    platform: {
+      spotify: "5AI4ZKsvVtfMTj0TknJjBE",
+      applemusic: "1658287059",
+      amazon: "B0BP837KCY",
+    },
   },
   {
     id: "laca-15612",
     name: " THE IDOLM@STER LIVE THE@TER FORWARD 02 BlueMoon Harmony",
     brand: "millionlive",
+    series: "LIVE THE@TER FORWARD",
     cover:
       "https://www.lantis.jp/imas/img/release_imgs__0007s_0001_LTF02_H1.png",
-    spotify: "5M22ADhFwe6Hhe5Xu0JGLi",
-    applemusic: "1658283629",
-    amazon: "B0BP7T6C27",
+    platform: {
+      spotify: "5M22ADhFwe6Hhe5Xu0JGLi",
+      applemusic: "1658283629",
+      amazon: "B0BP7T6C27",
+    },
   },
   {
     id: "laca-15613",
     name: "THE IDOLM@STER LIVE THE@TER FORWARD 03 Starlight Melody",
     brand: "millionlive",
+    series: "LIVE THE@TER FORWARD",
     cover:
       "https://www.lantis.jp/imas/img/release_imgs__0007s_0000_LTF03_H1.png",
-    spotify: "0dqNWSJ8ePef1E2HbmmnNH",
-    applemusic: "1658288008",
-    amazon: "B0BP7SQ2L7",
+    platform: {
+      spotify: "0dqNWSJ8ePef1E2HbmmnNH",
+      applemusic: "1658288008",
+      amazon: "B0BP7SQ2L7",
+    },
   },
   {
     id: "sun_rhythm_orchestra",
     name: "サンリズム・オーケストラ (Brand New Ver.)",
     brand: "millionlive",
+    series: "LIVE THE@TER FORWARD (Brand New Ver.)",
     cover:
       "https://m.media-amazon.com/images/I/61j7dm2YGRL._UX500_FMwebp_QL85_.jpg",
-    spotify: "0MxRdYIqMGX6KlskKC2tsK",
-    applemusic: "1658215534",
-    amazon: "B0BP6HRXXJ",
+    platform: {
+      spotify: "0MxRdYIqMGX6KlskKC2tsK",
+      applemusic: "1658215534",
+      amazon: "B0BP6HRXXJ",
+    },
   },
   {
     id: "brave_harmony",
     name: "brave HARMONY (Brand New Ver.)",
     brand: "millionlive",
+    series: "LIVE THE@TER FORWARD (Brand New Ver.)",
     cover:
       "https://m.media-amazon.com/images/I/51RB5fSHM7L._UX500_FMwebp_QL85_.jpg",
-    spotify: "5tcjU2eXkznY57KibCU0OD",
-    applemusic: "1658215347",
-    amazon: "B0BP6LZSG3",
+    platform: {
+      spotify: "5tcjU2eXkznY57KibCU0OD",
+      applemusic: "1658215347",
+      amazon: "B0BP6LZSG3",
+    },
   },
   {
     id: "starry_melody",
     name: "Starry Melody (Brand New Ver.)",
     brand: "millionlive",
+    series: "LIVE THE@TER FORWARD (Brand New Ver.)",
     cover:
       "https://m.media-amazon.com/images/I/51PVdwoe2BL._UX500_FMwebp_QL85_.jpg",
-    spotify: "4ScFE4kJaTJOtSGGAoEIZd",
-    applemusic: "1658288008",
-    amazon: "B0BP6M2GGX",
+    platform: {
+      spotify: "4ScFE4kJaTJOtSGGAoEIZd",
+      applemusic: "1658288008",
+      amazon: "B0BP6M2GGX",
+    },
   },
   {
     id: "refrain_kiss",
     name: "リフレインキス (Brand New Ver.)",
     brand: "millionlive",
+    series: "LIVE THE@TER FORWARD (Brand New Ver.)",
     cover:
       "https://m.media-amazon.com/images/I/61DcsnCH3XL._UX500_FMwebp_QL85_.jpg",
-    spotify: "5M761lw0DidVCkXofyhDu5",
-    applemusic: "1658262869",
-    amazon: "B0BP7HCMXQ",
+    platform: {
+      spotify: "5M761lw0DidVCkXofyhDu5",
+      applemusic: "1658262869",
+      amazon: "B0BP7HCMXQ",
+    },
   },
 ];
 
