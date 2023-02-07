f866e188678f494de34777f65c46f38de3b66e73
Lon Sagisawa <lon@sagisawa.me>
2023-02-07 23:20:58 +0900

    SC: New data structure for Synthe-Side

--- src/model/shinycolors/2021_SyntheSide.ts
+++ src/model/shinycolors/2021_SyntheSide.ts
@@ -8,9 +8,14 @@ const SyntheSide: Array<Album> = [
     brand: "shinycolors",
     cover:
       "https://shinycolors.lantis.jp/X9pa5jNY/wp-content/uploads/2021/12/LACM-24244.jpg",
-    spotify: "2AtVxlrMgKVDsrqA6onKYJ",
-    applemusic: "1627485946",
-    amazon: "B0B338Z762",
+    platform: {
+      spotify: "2AtVxlrMgKVDsrqA6onKYJ",
+      applemusic: "1627485946",
+      amazon: "B0B338Z762",
+    },
+    songs: [
+      "Killer×Mission",
+    ],
   },
   {
     id: "lacm-24245",
@@ -18,9 +23,14 @@ const SyntheSide: Array<Album> = [
     brand: "shinycolors",
     cover:
       "https://shinycolors.lantis.jp/X9pa5jNY/wp-content/uploads/2021/12/LACM-24245.jpg",
-    spotify: "7rg9RfJGUbH8aLeqHLGBst",
-    applemusic: "1633024722",
-    amazon: "B0B5TT4SND",
+    platform: {
+      spotify: "7rg9RfJGUbH8aLeqHLGBst",
+      applemusic: "1633024722",
+      amazon: "B0B5TT4SND",
+    },
+    songs: [
+      "相合学舎",
+    ],
   },
   {
     id: "lacm-24246",
@@ -28,9 +38,14 @@ const SyntheSide: Array<Album> = [
     brand: "shinycolors",
     cover:
       "https://shinycolors.lantis.jp/X9pa5jNY/wp-content/uploads/2022/01/LACM-24246.jpg",
-    spotify: "5VWEYE7ttilS4rRwoDk6HS",
-    applemusic: "1635865942",
-    amazon: "B0B7CPT85K",
+    platform: {
+      spotify: "5VWEYE7ttilS4rRwoDk6HS",
+      applemusic: "1635865942",
+      amazon: "B0B7CPT85K",
+    },
+    songs: [
+      "Secret utopIA",
+    ],
   },
 ];
 
