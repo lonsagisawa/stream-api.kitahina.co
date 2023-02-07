75eaade67e7fe0811bcf912529f3604e6c175f0a
Lon Sagisawa <lon@sagisawa.me>
2023-02-08 01:09:39 +0900

    ML: new data structure

--- src/model/millionlive/Other.ts
+++ src/model/millionlive/Other.ts
@@ -7,45 +7,55 @@ const Other: Array<Album> = [
     brand: "millionlive",
     cover:
       "https://m.media-amazon.com/images/I/51GTFtVTLuL._UX500_FMwebp_QL85_.jpg",
-    spotify: "2tVehWE9N04E1thhRXZoAo",
-    applemusic: "1658285082",
-    amazon: "B0BP7TCBGK",
+    platform: {
+      spotify: "2tVehWE9N04E1thhRXZoAo",
+      applemusic: "1658285082",
+      amazon: "B0BP7TCBGK",
+    },
   },
   {
     id: "lacm-24025",
     name: "なんどでも笑おう - ミリオンライブ！盤",
     brand: "millionlive",
     cover: "https://www.lantis.jp/imas/img/LACM-24025.jpg",
-    spotify: "1F2H6rXX1LzMHHQ4i9oT9w",
-    applemusic: "1658341245",
-    amazon: "B0BP8C3M1L",
+    platform: {
+      spotify: "1F2H6rXX1LzMHHQ4i9oT9w",
+      applemusic: "1658341245",
+      amazon: "B0BP8C3M1L",
+    },
   },
   {
     id: "lacm-24004",
     name: "THE IDOLM@STER MILLION LIVE! ZWEIGLANZ アライアンス・スターダスト",
     brand: "millionlive",
     cover: "https://www.lantis.jp/imas/img/LACM-24004.jpg",
-    spotify: "0p8qcX8zAMWjT5EZuTNIM4",
-    applemusic: "1658270317",
-    amazon: "B0BP7R2FSS",
+    platform: {
+      spotify: "0p8qcX8zAMWjT5EZuTNIM4",
+      applemusic: "1658270317",
+      amazon: "B0BP7R2FSS",
+    },
   },
   {
     id: "lacm-24159",
     name: "THE IDOLM@STER MILLION LIVE! 聖ミリオン女学園",
     brand: "millionlive",
     cover: "https://www.lantis.jp/imas/img/LACM-24159.jpg",
-    spotify: "3CbTpT9Kb9HAi60zdF4OlS",
-    applemusic: "1658310586",
-    amazon: "B0BP84YHNQ",
+    platform: {
+      spotify: "3CbTpT9Kb9HAi60zdF4OlS",
+      applemusic: "1658310586",
+      amazon: "B0BP84YHNQ",
+    },
   },
   {
     id: "lacm-24163",
     name: "VOY@GER - ミリオンライブ！盤",
     brand: "millionlive",
     cover: "https://www.lantis.jp/imas/img/LACM-24163_H1.jpg",
-    spotify: "5qK9kVY1VwTb2TfuK8oumE",
-    applemusic: "1658305728",
-    amazon: "B0BP83ZH6D",
+    platform: {
+      spotify: "5qK9kVY1VwTb2TfuK8oumE",
+      applemusic: "1658305728",
+      amazon: "B0BP83ZH6D",
+    },
   },
   {
     id: "clover_days",
@@ -53,9 +63,11 @@ const Other: Array<Album> = [
     brand: "millionlive",
     cover:
       "https://m.media-amazon.com/images/I/61wWSIJ30QL._UX500_FMwebp_QL85_.jpg",
-    spotify: "1ulBx1CY8eGJ8EtmuLp1EU",
-    applemusic: "1658342716",
-    amazon: "B0BP8BHV7F",
+    platform: {
+      spotify: "1ulBx1CY8eGJ8EtmuLp1EU",
+      applemusic: "1658342716",
+      amazon: "B0BP8BHV7F",
+    },
   },
 ];
 
