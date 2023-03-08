a00ccf228a36a7e55aaf9cd45b3397c0cf2a7846
Lon Sagisawa <lon@sagisawa.me>
2023-03-09 01:42:03 +0900

    Shiny Colors: Colorful Fe@thers

--- src/model/shinycolors/2021_ColorfulFeathers.ts
+++ src/model/shinycolors/2021_ColorfulFeathers.ts
@@ -3,6 +3,53 @@ import { Album } from "../../types/Album.ts";
 const ColorfulFeathers: Array<Album> = [
   /* COLORFUL FE@THERS */
   {
+    id: "laca-15861",
+    name: "THE IDOLM@STER SHINY COLORS COLORFUL FE@THERS -Stella-",
+    series: "COLORFUL FE@THERS",
+    brand: "shinycolors",
+    cover:
+      "https://shinycolors.lantis.jp/X9pa5jNY/wp-content/uploads/2021/12/LACA-15861.jpg",
+    platform: {
+      spotify: "4LNCaJ2SQh27tRtaZGzmT9",
+      applemusic: "1675339132",
+      amazon: "B0BXB9FM2X",
+    },
+    songs: [
+      "プラニスフィア ～planisphere～",
+      "ありったけの輝きで",
+      "星をめざして",
+      "夢見鳥",
+      "アポイント・シグナル",
+      "チョコデート・サンデー",
+      "Sweet Memories",
+      "ハナマルバッジ",
+    ],
+  },
+  {
+    id: "laca-15862",
+    name: "THE IDOLM@STER SHINY COLORS COLORFUL FE@THERS -Luna-",
+    series: "COLORFUL FE@THERS",
+    brand: "shinycolors",
+    cover:
+      "https://shinycolors.lantis.jp/X9pa5jNY/wp-content/uploads/2021/12/LACA-15862.jpg",
+    platform: {
+      spotify: "0PC9a1J3ae1N0yrTXJS2kT",
+      applemusic: "1675518486",
+      amazon: "B0BXFBD7F1",
+    },
+    songs: [
+      "リフレクトサイン",
+      "誰ソ彼アイデンティティー",
+      "わたしの主人公はわたしだから！",
+      "また明日",
+      "常咲の庭",
+      "プラスチック・アンブレラ",
+      "Going my way",
+      "雪・月・風・花",
+      "スローモーション",
+    ],
+  },
+  {
     id: "laca-15863",
     name: "THE IDOLM@STER SHINY COLORS COLORFUL FE@THERS -Sol-",
     series: "COLORFUL FE@THERS",
