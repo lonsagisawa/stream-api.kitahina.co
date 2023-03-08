3f7a579df11cfee0f347974b21db3e61b26ea7b5
Lon Sagisawa <lon@sagisawa.me>
2023-03-09 02:18:23 +0900

    Shiny Colors: Fr@gment Wing

--- src/model/ShinyColors.ts
+++ src/model/ShinyColors.ts
@@ -1,5 +1,6 @@
 import { Album } from "../types/Album.ts";
 import BrilliantWing from "./shinycolors/2018_BrilliantWing.ts";
+import FragmentWing from "./shinycolors/2019_FragmentWing.ts";
 import ColorfulFeathers from "./shinycolors/2021_ColorfulFeathers.ts";
 import SyntheSide from "./shinycolors/2021_SyntheSide.ts";
 import PanoramaWing from "./shinycolors/2022_PanoramaWing.ts";
@@ -9,6 +10,7 @@ const Albums: Array<Album> = [];
 
 const ShinyColors: Array<Album> = Albums.concat(
   BrilliantWing,
+  FragmentWing,
   ColorfulFeathers,
   SyntheSide,
   PanoramaWing,
