06cd389042a1d431fac5f01e55959b1fc8408ba7
Lon Sagisawa <lon@sagisawa.me>
2023-03-09 01:57:05 +0900

    Shiny Colors: Brilli@nt Wing

--- src/model/ShinyColors.ts
+++ src/model/ShinyColors.ts
@@ -1,4 +1,5 @@
 import { Album } from "../types/Album.ts";
+import BrilliantWing from "./shinycolors/2018_BrilliantWing.ts";
 import ColorfulFeathers from "./shinycolors/2021_ColorfulFeathers.ts";
 import SyntheSide from "./shinycolors/2021_SyntheSide.ts";
 import PanoramaWing from "./shinycolors/2022_PanoramaWing.ts";
@@ -7,6 +8,7 @@ import Other from "./shinycolors/Other.ts";
 const Albums: Array<Album> = [];
 
 const ShinyColors: Array<Album> = Albums.concat(
+  BrilliantWing,
   ColorfulFeathers,
   SyntheSide,
   PanoramaWing,
---
+++ src/model/shinycolors/2018_BrilliantWing.ts
@@ -0,0 +1,93 @@
+import { Album } from "../../types/Album.ts";
+
+const BrilliantWing: Array<Album> = [
+  /* BRILLI@NT WING */
+  {
+    id: "lacm-14781",
+    name: "THE IDOLM@STER SHINY COLORS BRILLI@NT WING 01 Spread the Wings!!",
+    series: "BRILLI@NT WING",
+    brand: "shinycolors",
+    cover:
+      "https://shinycolors.lantis.jp/X9pa5jNY/wp-content/uploads/2021/12/jk_LACM-14781.jpg",
+    platform: {
+      spotify: "2klsGa0KCtEePa04pjbV7r",
+      applemusic: "1675421830",
+      amazon: "B0BXBC351C",
+    },
+    songs: [
+      "Spread the Wings!!",
+      "Multicolored Sky",
+    ],
+  },
+  {
+    id: "lacm-14782",
+    name: "THE IDOLM@STER SHINY COLORS BRILLI@NT WING 02 ヒカリのdestination",
+    series: "BRILLI@NT WING",
+    brand: "shinycolors",
+    cover:
+      "https://shinycolors.lantis.jp/X9pa5jNY/wp-content/uploads/2021/12/LACM-14782.jpg",
+    platform: {
+      spotify: "5pV7PhnoZheAalJimumERq",
+      applemusic: "1675480422",
+      amazon: "B0BXBBYCW9",
+    },
+    songs: [
+      "ヒカリのdestination",
+      "虹になれ",
+    ],
+  },
+  {
+    id: "lacm-14783",
+    name:
+      "THE IDOLM@STER SHINY COLORS BRILLI@NT WING 03 バベルシティ・グレイス",
+    series: "BRILLI@NT WING",
+    brand: "shinycolors",
+    cover:
+      "https://shinycolors.lantis.jp/X9pa5jNY/wp-content/uploads/2021/12/LACM-14783.jpg",
+    platform: {
+      spotify: "2eB8FxEqOAXh2Sl8gwmLBa",
+      applemusic: "1675851858",
+      amazon: "B0BXFHMPV5",
+    },
+    songs: [
+      "バベルシティ・グレイス",
+      "幻惑SILHOUETTE",
+    ],
+  },
+  {
+    id: "lacm-14784",
+    name: "THE IDOLM@STER SHINY COLORS BRILLI@NT WING 04 夢咲きAfter school",
+    series: "BRILLI@NT WING",
+    brand: "shinycolors",
+    cover:
+      "https://shinycolors.lantis.jp/X9pa5jNY/wp-content/uploads/2021/12/LACM-14784.jpg",
+    platform: {
+      spotify: "7nbtHmpCxyd61CvO3DstSu",
+      applemusic: "1675550818",
+      amazon: "B0BXBDCP6S",
+    },
+    songs: [
+      "夢咲きAfter school",
+      "太陽キッス",
+    ],
+  },
+  {
+    id: "lacm-14785",
+    name: "THE IDOLM@STER SHINY COLORS BRILLI@NT WING 05 アルストロメリア",
+    series: "BRILLI@NT WING",
+    brand: "shinycolors",
+    cover:
+      "https://shinycolors.lantis.jp/X9pa5jNY/wp-content/uploads/2021/12/LACM-14785.jpg",
+    platform: {
+      spotify: "0zRTx2t73zMaA7LuGevhdJ",
+      applemusic: "1675550939",
+      amazon: "B0BXBDVDXN",
+    },
+    songs: [
+      "アルストロメリア",
+      "ハピリリ",
+    ],
+  },
+];
+
+export default BrilliantWing;
