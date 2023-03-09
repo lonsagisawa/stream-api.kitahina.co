56a354d027d00061321f399e55d6cf45f935d708
Lon Sagisawa <lon@sagisawa.me>
2023-03-09 23:16:21 +0900

    Shiny Colors: Wing Collection

---
+++ src/model/shinycolors/2023_WingCollection.ts
@@ -0,0 +1,35 @@
+import { Album } from "../../types/Album.ts";
+
+const WingCollection: Array<Album> = [
+  /* OFF VOCAL COLLECTION */
+  {
+    id: "laca-9946",
+    name: "THE IDOLM@STER SHINY COLORS WING COLLECTION -A side-",
+    series: "WING COLLECTION",
+    brand: "shinycolors",
+    cover:
+      "https://shinycolors.lantis.jp/X9pa5jNY/wp-content/uploads/2022/10/LACA-9946_7.jpg",
+    platform: {
+      spotify: "5YyUeirmIV0Uo1cqf6Wi5x",
+      applemusic: "1665694055",
+      amazon: "B0BSG1DD73",
+    },
+    songs: [],
+  },
+  {
+    id: "laca-9948",
+    name: "THE IDOLM@STER SHINY COLORS WING COLLECTION -B side-",
+    series: "WING COLLECTION",
+    brand: "shinycolors",
+    cover:
+      "https://shinycolors.lantis.jp/X9pa5jNY/wp-content/uploads/2022/10/LACA-9948_9.jpg",
+    platform: {
+      spotify: "018dNsC2qYMZPWQlBYaHUk",
+      applemusic: "1670365372",
+      amazon: "B0BV2H6X8V",
+    },
+    songs: [],
+  },
+];
+
+export default WingCollection;
