a444fb7e8b9e793c4cbf658da9fde390bede3168
Lon Sagisawa <lon@sagisawa.me>
2023-03-09 23:07:58 +0900

    Shiny Colors: L@yered Wing, Off Vocal Collection

--- src/model/ShinyColors.ts
+++ src/model/ShinyColors.ts
@@ -2,8 +2,10 @@ import { Album } from "../types/Album.ts";
 import BrilliantWing from "./shinycolors/2018_BrilliantWing.ts";
 import FragmentWing from "./shinycolors/2019_FragmentWing.ts";
 import GradateWing from "./shinycolors/2020_GradateWing.ts";
+import LayeredWing from "./shinycolors/2021_LayeredWing.ts";
 import ColorfulFeathers from "./shinycolors/2021_ColorfulFeathers.ts";
 import SyntheSide from "./shinycolors/2021_SyntheSide.ts";
+import OffVocalCollection from "./shinycolors/2022_OffVocalCollection.ts";
 import PanoramaWing from "./shinycolors/2022_PanoramaWing.ts";
 import Other from "./shinycolors/Other.ts";
 
@@ -13,8 +15,10 @@ const ShinyColors: Array<Album> = Albums.concat(
   BrilliantWing,
   FragmentWing,
   GradateWing,
+  LayeredWing,
   ColorfulFeathers,
   SyntheSide,
+  OffVocalCollection,
   PanoramaWing,
   Other,
 );
---
+++ src/model/shinycolors/2022_OffVocalCollection.ts
@@ -0,0 +1,35 @@
+import { Album } from "../../types/Album.ts";
+
+const OffVocalCollection: Array<Album> = [
+  /* OFF VOCAL COLLECTION */
+  {
+    id: "laca-9872",
+    name: "THE IDOLM@STER SHINY COLORS OFF VOCAL COLLECTION 01",
+    series: "OFF VOCAL COLLECTION",
+    brand: "shinycolors",
+    cover:
+      "https://shinycolors.lantis.jp/X9pa5jNY/wp-content/uploads/2021/12/LACA-9872_4.jpeg",
+    platform: {
+      spotify: "1F9N0jDwLcY7PfSDtoD7E4",
+      applemusic: "1675432286",
+      amazon: "B0BXBJS2NS",
+    },
+    songs: [],
+  },
+  {
+    id: "laca-9944",
+    name: "THE IDOLM@STER SHINY COLORS OFF VOCAL COLLECTION 02",
+    series: "OFF VOCAL COLLECTION",
+    brand: "shinycolors",
+    cover:
+      "https://shinycolors.lantis.jp/X9pa5jNY/wp-content/uploads/2022/09/LACA-9944_45.jpg",
+    platform: {
+      spotify: "6mxa6iWN8ria6caDkU8pgs",
+      applemusic: "1658268662",
+      amazon: "B0BP7PFDHM",
+    },
+    songs: [],
+  },
+];
+
+export default OffVocalCollection;
