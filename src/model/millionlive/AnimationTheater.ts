5a423a8c458ae6a7de018827d85d83e0ccc9b8dd
Lon Sagisawa <lon@sagisawa.me>
2023-10-08 00:27:58 +0900

    ミリオンライブ！ - 2023/8新譜

---
+++ src/model/millionlive/AnimationTheater.ts
@@ -0,0 +1,22 @@
+import { Album } from "../../types/Album.ts";
+
+const AnimationTheater: Array<Album> = [
+  {
+    id: "lacm-24420",
+    name: "Rat A Tat!!!",
+    brand: "millionlive",
+    series: "MILLION ANIMATION THE@TER",
+    cover: "https://www.lantis.jp/imas/img/LACM-24420.jpg",
+    platform: {
+      spotify: "1s2GWfVfNDk453enM4jgcZ",
+      applemusic: "1701119988",
+      amazon: "B0CDQ9SKC3",
+    },
+    songs: [
+      "Rat a Tat!!!",
+      "セブンカウント",
+    ],
+  },
+];
+
+export default AnimationTheater;
