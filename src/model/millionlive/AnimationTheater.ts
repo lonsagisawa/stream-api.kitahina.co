e749b340bd71ed0ce9454fb616542afa03ba33ef
Lon Sagisawa <lon@sagisawa.me>
2023-10-20 01:07:41 +0900

    Million Live!: Oct 2023

--- src/model/millionlive/AnimationTheater.ts
+++ src/model/millionlive/AnimationTheater.ts
@@ -17,6 +17,51 @@ const AnimationTheater: Array<Album> = [
       "セブンカウント",
     ],
   },
+  {
+    id: "lacm-24421",
+    name: "Team1st『Star Impression』",
+    brand: "millionlive",
+    series: "MILLION ANIMATION THE@TER",
+    cover: "https://www.lantis.jp/imas/img/LACM-24421.jpg",
+    platform: {
+      spotify: "2siLu2J6gKEWxrJc9tLlCr",
+      applemusic: "1706820190",
+      amazon: "B0CHRZZSRG",
+    },
+    songs: [
+      "Star Impression",
+    ],
+  },
+  {
+    id: "lacm-24422",
+    name: "Team2nd『海風とカスタネット』",
+    brand: "millionlive",
+    series: "MILLION ANIMATION THE@TER",
+    cover: "https://www.lantis.jp/imas/img/LACM-24422.jpg",
+    platform: {
+      spotify: "04iPc4MR7NlGMnlG6dVBKt",
+      applemusic: "1706820875",
+      amazon: "B0CHS39K6S",
+    },
+    songs: [
+      "海風とカスタネット",
+    ],
+  },
+  {
+    id: "lacm-24424",
+    name: "Team4th『catch my feeling』",
+    brand: "millionlive",
+    series: "MILLION ANIMATION THE@TER",
+    cover: "https://www.lantis.jp/imas/img/LACM-24424.jpg",
+    platform: {
+      spotify: "7BTPD2OgwDIQRVtGqkQ9yo",
+      applemusic: "1706820159",
+      amazon: "B0CHRZYQCK",
+    },
+    songs: [
+      "catch my feeling",
+    ],
+  },
 ];
 
 export default AnimationTheater;
