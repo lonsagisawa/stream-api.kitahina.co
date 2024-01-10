bbafae6a4d3ca756e3a1244d2e4c0717766c2b6a
Lon Sagisawa <lon@sagisawa.me>
2024-01-11 01:43:40 +0900

    Shiny Colors: CA07, CA08

--- src/model/shinycolors/2023_Canvas.ts
+++ src/model/shinycolors/2023_Canvas.ts
@@ -14,11 +14,7 @@ const Canvas: Array<Album> = [
       applemusic: "1679823084",
       amazon: "B0BZWPG9DF",
     },
-    songs: [
-      "Forward March!!!",
-      "星が流れて",
-      "BRIGHTEST WHITE",
-    ],
+    songs: ["Forward March!!!", "星が流れて", "BRIGHTEST WHITE"],
   },
   {
     id: "lacm-24362",
@@ -32,11 +28,7 @@ const Canvas: Array<Album> = [
       applemusic: "1686941736",
       amazon: "B0C4LCMV7G",
     },
-    songs: [
-      "とある英雄たちの物語",
-      "Unsung Heroes",
-      "有彩色ユリイカ",
-    ],
+    songs: ["とある英雄たちの物語", "Unsung Heroes", "有彩色ユリイカ"],
   },
   {
     id: "lacm-24363",
@@ -50,11 +42,7 @@ const Canvas: Array<Album> = [
       applemusic: "1689767801",
       amazon: "B0C6FHVYRH",
     },
-    songs: [
-      "ハナサカサイサイ",
-      "全力アンサー",
-      "ひめくりモザイク",
-    ],
+    songs: ["ハナサカサイサイ", "全力アンサー", "ひめくりモザイク"],
   },
   {
     id: "lacm-24364",
@@ -68,11 +56,7 @@ const Canvas: Array<Album> = [
       applemusic: "1694293919",
       amazon: "B0C9BZJRV6",
     },
-    songs: [
-      "メッセージ",
-      "Love Letter",
-      "グラデーション",
-    ],
+    songs: ["メッセージ", "Love Letter", "グラデーション"],
   },
   {
     id: "lacm-24365",
@@ -86,11 +70,7 @@ const Canvas: Array<Album> = [
       applemusic: "1699417403",
       amazon: "B0CCW5XFYD",
     },
-    songs: [
-      "Imitation Ghost",
-      "BURN BURN",
-      "Start up Stand up",
-    ],
+    songs: ["Imitation Ghost", "BURN BURN", "Start up Stand up"],
   },
   {
     id: "lacm-24366",
@@ -104,11 +84,35 @@ const Canvas: Array<Album> = [
       applemusic: "1707372645",
       amazon: "B0CJ382CTN",
     },
-    songs: [
-      "Reflection",
-      "夢が夢じゃなくなるその日まで",
-      "青とオレンジ",
-    ],
+    songs: ["Reflection", "夢が夢じゃなくなるその日まで", "青とオレンジ"],
+  },
+  {
+    id: "lacm-24367",
+    name: 'THE IDOLM@STER SHINY COLORS "CANVAS" 07',
+    series: '"CANVAS"',
+    brand: "shinycolors",
+    cover:
+      "https://shinycolors.lantis.jp/X9pa5jNY/wp-content/uploads/2023/09/LACM-24367.jpg",
+    platform: {
+      spotify: "65qZAuFzFHcbU0KZMy2q3z",
+      applemusic: "1710011695",
+      amazon: "B0CK8G3YL7",
+    },
+    songs: ["Forbidden Paradise", "SWEETEST BITE", "White Story"],
+  },
+  {
+    id: "lacm-24368",
+    name: 'THE IDOLM@STER SHINY COLORS "CANVAS" 08',
+    series: '"CANVAS"',
+    brand: "shinycolors",
+    cover:
+      "https://shinycolors.lantis.jp/X9pa5jNY/wp-content/uploads/2023/10/LACM-24368.jpg",
+    platform: {
+      spotify: "1yHDHmI9GOKVa15ye4sYk8",
+      applemusic: "1713420580",
+      amazon: "B0CLPLNVPB",
+    },
+    songs: ["無自覚アプリオリ", "くだらないや", "平行線の美学"],
   },
 ];
 
